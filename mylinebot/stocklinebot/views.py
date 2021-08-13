0bafb360bb543a78ca81b76e66570e3f005e1f17
tom <tom1128lin@gmail.com>
2021-08-13 22:18:23 +0800

    fix bug

--- mylinebot/stocklinebot/views.py
+++ mylinebot/stocklinebot/views.py
@@ -17,27 +17,7 @@ parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
  
 @csrf_exempt
 def callback(request):
- 
-    '''if request.method == 'POST':
-        signature = request.META['HTTP_X_LINE_SIGNATURE']
-        body = request.body.decode('utf-8')
- 
-        try:
-            events = parser.parse(body, signature)  # 傳入的事件
-        except InvalidSignatureError:
-            return HttpResponseForbidden()
-        except LineBotApiError:
-            return HttpResponseBadRequest()
- 
-        for event in events:
-            if isinstance(event, MessageEvent):  # 如果有訊息事件
-                line_bot_api.reply_message(  # 回復傳入的訊息文字
-                    event.reply_token,
-                    TextSendMessage(text=event.message.text)
-                )
-        return HttpResponse()
-    else:
-        return HttpResponseBadRequest()'''
+
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
@@ -63,23 +43,24 @@ def callback(request):
                 content = ''
 
                 stock_rt = twstock.realtime.get(text)
-                my_datetime = datetime.fromtimestamp(stock_rt['timestamp']+8*60*60)
+                stock = twstock.Stock(text)#twstock.Stock('2330')
+
+                my_datetime = datetime.fromtimestamp(stock_rt['timestamp'])
                 my_time = my_datetime.strftime('%H:%M:%S')
 
                 content += '%s (%s) %s\n' %(
                     stock_rt['info']['name'],
                     stock_rt['info']['code'],
                     my_time)
-                content += '現價: %s / 開盤: %s\n'%(
+                content += '現價: %s / 幅度: %s\n'%(
                     stock_rt['realtime']['latest_trade_price'],
-                    stock_rt['realtime']['open'])
+                    format(float(stock_rt['realtime']['latest_trade_price']) - stock.data[-2][6],'.2f'))
+                print(float(stock_rt['realtime']['latest_trade_price']),stock.data[-2][6])
                 content += '最高: %s / 最低: %s\n' %(
                     stock_rt['realtime']['high'],
                     stock_rt['realtime']['low'])
-                content += '量: %s\n' %(stock_rt['realtime']['accumulate_trade_volume'])
-
-                stock = twstock.Stock(text)#twstock.Stock('2330')
-                content += '-----\n'
+                content += '量: %s\n' %(stock_rt['realtime']['accumulate_trade_volume'])            
+                content += '--------------\n'
                 content += '最近五日價格: \n'
                 price5 = stock.price[-5:][::-1]
                 date5 = stock.date[-5:][::-1]
